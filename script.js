808d8b7ccd09d11b3d4b7e067c44988264ace251
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-08-31 12:07:44 +0530

    Login Page and Psychological Test Page added

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const loadingScreen = document.querySelector("#loading-screen");
-    const loadHeading = document.querySelector('.load');
-    setTimeout(()=>{
-        loadingScreen.style.display = "none";
-        loadHeading.style.display = 'none';
-    },1000);
+    const loadingScreen = document.querySelector(".loading");
+    const mainData = document.querySelector(".main-content");
+    
+    if (loadingScreen) {
+        setTimeout(() => {
+            loadingScreen.style.transition = 'opacity 0.5s ease';
+            loadingScreen.style.opacity = 0;
+
+            setTimeout(() => {
+                loadingScreen.style.display = 'none';
+                mainData.style.display = "block";
+            }, 1000);  // Wait for the transition to finish before hiding
+        }, 4000);
+    }
 });
+
+// document.addEventListener("DOMContentLoaded",()=>{
+//     let head = document.querySelector("#main-head");
+//     let text = head.textContent;
+//     head.innerHTML = "";
+
+//     let i = 0;
+//     function autoType(){
+//         if(i<text.length){
+//             head.innerHTML += text.charAt(i);
+//             i++;
+//         }
+//         if(i<text.length){
+//             setTimeout(autoType,100);
+//         }
+//     }
+//    autoType();
+// })
