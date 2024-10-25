3d0fb301df61a48f0995c8e0cfa67637994d391e
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-10-25 19:20:26 +0530

    Account Section added using Web Storage

---
+++ Frontend/Account/profile.js
@@ -0,0 +1,21 @@
+
+
+function mskPassword(password){
+    return '*'.repeat(password.length);
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const userName = localStorage.getItem('name');
+    const pwd = localStorage.getItem('password');
+    const email = localStorage.getItem('email');
+    const maskedPwd = mskPassword(pwd);
+    if(userName){
+        document.getElementById("userNameDisplay").innerText = userName;
+    }
+    if(pwd){
+        document.getElementById("pwdDisplay").innerText = maskedPwd;
+    }
+    if(email){
+        document.getElementById("emailDisplay").innerText = email;
+    }
+})
